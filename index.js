784ca5bba97ff78dac5d0d88d545d62006577cfe
Marcy Ordinario <aimlesscontrivance@gmail.com>
2025-05-18 13:24:58 -0700

    Added stats.

--- index.js
+++ index.js
@@ -25,18 +25,123 @@
 
 // Declare card template
 const Template = document.getElementById("card-template");
-console.log(["Template:", Template]);
+const GameGrid = document.getElementById("game_grid");
+// Declare difficulties
+const Difficulties = {
+    easy: {
+        cardAmount: 6,
+        timerSeconds: 15
+    },
+    medium: {
+        cardAmount: 12,
+        timerSeconds: 20
+    },
+    hard: {
+        cardAmount: 18,
+        timerSeconds: 25
+    }
+}
+// Declare difficulty (for this game)
+var difficulty = "easy";
 // Declare amount of cards
-const cards = 6;
+const cardAmount = Difficulties[difficulty].cardAmount;
+var clickAmount = 0;
+var pairsLeft = cardAmount / 2;
 
-async function setup () {
+async function setup() {
+    // Set cards up
+    await setupCards();
 
+    // Matching functionality
+    let firstCard = undefined
+    let secondCard = undefined
+    // When clicked
+    $(".card").on(("click"), function () {
+        // If cards are currently being compared,
+        // OR if the game has been lost,
+        // do nothing
+        if($(this).parent().hasClass("paused") ||
+           $(this).parent().hasClass("lost"))
+        {
+            return;
+        }
+        // Else, flip card
+        $(this).toggleClass("flip");
+        clickAmount++;
+        document.getElementById("clicks").innerText = `Clicks: ${clickAmount}`
+
+        // If first card is undefined,
+        // set the first card to the clicked card
+        if (!firstCard)
+        {
+            firstCard = $(this).find(".front_face")[0]
+            console.log(firstCard)
+            return;
+        }
+        // Else,
+        // set the second card to the clicked card
+        // AND check if the cards match
+        secondCard = $(this).find(".front_face")[0]
+        console.log(firstCard, secondCard);
+        // If the cards' ids match (i.e. same card),
+        // do nothing
+        if(firstCard.parentNode.id == secondCard.parentNode.id)
+        {
+            firstCard = undefined
+            return;
+        }
+        // If the cards' images match
+        if (firstCard.src == secondCard.src)
+        {
+            // Disable the cards' clicking
+            console.log("match")
+            pairsLeft--;
+            document.getElementById("pairs").innerText = `Pairs left: ${pairsLeft}`
+            $(`#${firstCard.parentNode.id}`).off("click")
+            $(`#${secondCard.parentNode.id}`).off("click")
+            // Reset selected cards
+            firstCard = undefined
+            secondCard = undefined
+            // Check if game won
+            checkIfGameWon();
+            return
+        }
+        // Else, if they don't
+        console.log("no match")
+        // Create temp variables to
+        // hold card objects
+        let tempFirstCard = firstCard
+        let tempSecondCard = secondCard
+        // Reset selected cards
+        firstCard = undefined
+        secondCard = undefined
+        // Add "paused" class to
+        // game grid to pause card selection
+        GameGrid.classList.add("paused")
+        // In one second, flip cards over and
+        // remove "paused" class
+        setTimeout(() => {
+            $(`#${tempFirstCard.parentNode.id}`).toggleClass("flip")
+            $(`#${tempSecondCard.parentNode.id}`).toggleClass("flip")
+            GameGrid.classList.remove("paused")
+        }, 1000)
+    });
+
+    // Set up timer
+    setupTimer();
+    // Set up stats
+    setupStats();
+}
+
+// Sets up pokemon cards
+async function setupCards()
+{
     // Get pokemon
     let pkmnRes = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=1025`);
     let pkmnsJson = await pkmnRes.json();
     let allPkmn = pkmnsJson.results;
-    let cards = [];
 
+    // Set chosen pokemon indexes
     const chosenIndexes = [];
     for(let i = 1; i <= 3; i++)
     {
@@ -44,13 +149,17 @@ async function setup () {
         chosenIndexes.push(index);
     }
 
+    // Array of cards
+    let cards = [];
     // For every card
     for(let i = 1; i <= 6; i++)
     {
         // Clone template node
         let card = Template.content.cloneNode(true);
         card.querySelector(".card").id = `img${i}`;
-        // Get (unique) index
+        // Get index
+        // (math function guarantees each
+        //  unique pokemon gets two cards each)
         let index = chosenIndexes[Math.floor((i - 1) / 2)]
         // Get results for pokemon at that index
         let name = allPkmn[index].name;
@@ -62,57 +171,80 @@ async function setup () {
         // Push card
         cards.push(card)
     }
-
+    // Shuffle cards
     shuffle(cards)
-
-    for(let i = 0; i < 6; i++)
+    // Append cards to game grid
+    for(let i = 0; i < cardAmount; i++)
     {
-        document.getElementById("game_grid").appendChild(cards[i]);
+        GameGrid.appendChild(cards[i]);
     }
+}
 
-    // Matching functionality
-    let firstCard = undefined
-    let secondCard = undefined
-    $(".card").on(("click"), function () {
-        $(this).toggleClass("flip");
+// Checks if the all cards are
+// flipped over (and, therefore,
+// if the game has been won)
+function checkIfGameWon()
+{
+    // If any cards aren't flipped over,
+    // then the game is not done yet
+    let solved = true;
+    for(let i = 1; i <= cardAmount; i++)
+    {
+        let newCard = GameGrid.querySelector(`#img${i}`)
+        if(!newCard.classList.contains("flip"))
+        {
+            solved = false;
+            break;
+        }
+    }
+    // If all cards are flipped over,
+    // then the game is won
+    if(solved == true)
+    {
+        GameGrid.classList.add("solved")
+        let winMessage = document.createElement("p");
+        winMessage.classList.add("message");
+        winMessage.innerText = "You win!";
+        document.getElementById("message_grid").appendChild(winMessage);
+        GameGrid.classList.add("won");
+    }
+}
 
-        // If first card is undefined,
-        // set the first card to the clicked card
-        if (!firstCard)
+// Sets up timer
+function setupTimer()
+{
+    // Set timer
+    let timerSeconds = Difficulties[difficulty].timerSeconds;
+    let secondsLeft = timerSeconds;
+    let secondDelay = 1000;
+    let timer = setInterval(() => {
+        // If game was won, stop counting down
+        if(GameGrid.classList.contains("won"))
         {
-            firstCard = $(this).find(".front_face")[0]
-            console.log(firstCard)
+            timer = clearInterval(timer)
+            return
         }
-        // Else,
-        // set the second card to the clicked card
-        // AND check if the cards match
-        else
+        secondsLeft--;
+        document.getElementById("timer").innerText = "Time left: " + secondsLeft
+        // If timer runs out
+        if(secondsLeft == 0)
         {
-            secondCard = $(this).find(".front_face")[0]
-            console.log(firstCard, secondCard);
-            // If the cards' images match
-            if (firstCard.src == secondCard.src)
-            {
-                console.log("match")
-                $(`#${firstCard.parentNode.id}`).off("click")
-                $(`#${secondCard.parentNode.id}`).off("click")
-                firstCard = undefined
-                secondCard = undefined
-            }
-            // Else, if they don't
-            else
-            {
-                console.log("no match")
-                console.log()
-                setTimeout(() => {
-                $(`#${firstCard.parentNode.id}`).toggleClass("flip")
-                $(`#${secondCard.parentNode.id}`).toggleClass("flip")
-                firstCard = undefined
-                secondCard = undefined
-                }, 1000)
-            }
+            timer = clearInterval(timer)
+            let loseMessage = document.createElement("p");
+            loseMessage.classList.add("message");
+            loseMessage.innerText = "You lose!";
+            document.getElementById("message_grid").appendChild(loseMessage);
+            GameGrid.classList.add("lost");
         }
-    });
+    }, secondDelay);
+}
+
+function setupStats()
+{
+    document.getElementById("difficulty").innerText = `Difficulty: ${difficulty}`
+    document.getElementById("timer").innerText = `Time left: ${Difficulties[difficulty].timerSeconds}`
+    document.getElementById("pairs").innerText = `Pairs left: ${cardAmount / 2}`
+    document.getElementById("clicks").innerText = `Clicks: 0`
 }
 
 // Shuffles an array
@@ -134,7 +266,7 @@ function shuffle(array) {
   }
 }
 
-$( document ).ready(function() {
+$(document).ready(function() {
     console.log( "ready!" );
 });
 
